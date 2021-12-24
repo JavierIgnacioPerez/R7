5cb0f2d992034298b17da7493533d31ad45f198d
lMumen <dementrox.matias@gmail.com>
2021-12-24 12:56:59 -0300

    style de pana

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,7 +6,7 @@ class LandingController < ApplicationController
     def sign_in
       user_email = params[:email]
       if user_email.exclude? 'avispa.tech'
-        return redirect_to 'landing#home'
+        return redirect_to root_path
       end
       cookies[:user_email] = user_email
       cookies[:user_name] = user_email.split('@')[0].capitalize
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
 
   # GET /messages or /messages.json
   def index
-    @messages = Message.all.order(created_at: :asc)    
+    @messages = Message.all.order(created_at: :asc)
     @system_message = SystemMessage.create( message: 'new connection', connections: ActionCable.server.connections.length ) if SystemMessage.first.blank?
     if SystemMessage.first.present?
       SystemMessage.first.update( connections: ActionCable.server.connections.length )
